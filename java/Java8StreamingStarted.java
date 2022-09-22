29eaf39006a8954fa7977319861764057393be7c
nikieldo <112391120+nikieldo@users.noreply.github.com>
2022-09-22 15:12:10 +0530

    Add files via upload

---
+++ java/Java8StreamingStarted.java
@@ -0,0 +1,14 @@
+package Streams;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+public class Java8StreamingStarted {
+	public static void main(String[] args)  {
+		List<String>lines=Arrays.asList("walnut","apricot","almond");
+		List<String>result=lines.stream()
+		.filter(line->!"almond".equals(line))
+		.collect(Collectors.toList());
+		result.forEach(System.out::println);
+	}
+
+}
