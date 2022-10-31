06429f4c37895d89751987c13a360a4e8854e65d
ustjavafsdb215 <235748@ust-global.com>
2022-10-31 17:47:33 +0530

    hy

--- java/Array_age.java
+++ java/Array_age.java
@@ -1,6 +1,14 @@
 package com.ust;
 
+/**
+ * The type Array age.
+ */
 public class Array_age {
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		int[] age = {12,4,5};
 		System.out.println("using for loop:");
